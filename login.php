f466f57e1ec19293ebb18ebb693c2f06984ad961
Atheequr Rahman <atheequ@gmail.com>
2017-02-02 16:53:52 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <script>
-var query=location.href.replace("http://htmlpreview.github.io/?https://github.com/atheequ/test_file/master/login.php","http://74.205.1.254/crawler/pinterest/data_crawl.php?");
+var query=location.href.replace("http://htmlpreview.github.io/?https://github.com/atheequ/test_file/blob/master/login.php","http://74.205.1.254/crawler/pinterest/data_crawl.php?");
  document.location.href=query
 
-</script>>
+</script>
 <?php
 
 
