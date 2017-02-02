ec5162e34ac3544b95eef9f63f54f212543bc4d6
Atheequr Rahman <atheequ@gmail.com>
2017-02-02 17:04:12 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <script>
-var query=location.href.replace("https://cdn.rawgit.com/atheequ/test_file/blob/master/login.php","http://74.205.1.254/crawler/pinterest/data_crawl.php?");
+var query=location.href.replace("https://cdn.rawgit.com/atheequ/test_file/master/login.php","http://74.205.1.254/crawler/pinterest/data_crawl.php?");
   document.location.href=query
 //alert(query);
 </script>
